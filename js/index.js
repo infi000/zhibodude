5437c8c52cf9caa900da311814e8389c673692e1
infi000-home <113079767@qq.com>
2016-12-13 01:04:30 +0800

    kingjames

--- js/index.js
+++ js/index.js
@@ -169,30 +169,43 @@ $(document).ready(function() {
     $(".liveChannels").on("click", function() {
         // console.log(13);
         var title = $(this).attr("dataType");
-        var channels = index.liveData.channels;
+        var channels = {
+            "CCTV—5":"http://cctv5.vtime.cntv.dnion.com:8000/live/no/211_/seg0/index.m3u8",
+            "五星体育":"http://183.207.255.190/live/program/live/ssty/2300000/mnf.m3u8",
+            "广东体育":"http://zb.iptv.gd.cn:30001/183.58.12.212:30001/PLTV/88888905/224/3221227521/1.m3u8",
+            "北京体育":"http://btv6.vtime.cntv.cloudcdn.net:8500/cache/314_/seg0/index.m3u8",
+            "青海卫视":"http://223.75.3.82:8081/live/live3/QHWS/2000/QHWS-2000-node1.m3u8?fmt=x264_2000k_mpegts&authType=2&node=1&type=live",
+        }
         var URL;
-        for (key in channels) {
-            if (channels[key].type == 0) {
-                if (channels[key].title == title) {
-                    URL = channels[key].url;
-                    console.log("key", key);
-                    console.log("url", URL);
-                    console.log("type", channels[key].type);
-                    break;
-                };
-            } else if (channels[key].type == 1) {
-                if (title == "风云足球") {
-                    URL = "http%3A%2F%2F124.192.145.197%2Fm3u8%2Ffyzq_1300%2Fdesc.m3u8%3Fstream_id%3Dfyzq_1300%26path%3D124.192.145.194%2C124.192.145.66%2C124.192.145.154%26amltag%3D202%26mltag%3D202%26platid%3D10%26splatid%3D1003%26qos%3D4%26fcheck%3D0%26uid%3D2093832770.rp%26keyitem%3DdpoOd-l3AnupIUuBI8VtFwmKzWltcfnZpngeV5Yo3I1Oleh6yGrbVeKRoRQ.%26ntm%3D1480430400%26nkey2%3D6af0f495d57059bbc2ea9c8868d0851e%26nkey%3D2067e8bde51570a85ff460e0978b0971%26tag%3Dlive%26video_type%3Dm3u8%26useloc%3D0%26mslice%3D3%26uidx%3D0%26errc%3D0%26gn%3D149%26lrtmcd%3D109%26buss%3D202%26cips%3D124.205.90.66%26geo%3DCN-1-12-16%26tmn%3D1480411885%26pnl%3D149%2C149%2C398%2C224%26ext%3Dm3u8%26sign%3Dlive_web%26ostype%3Dmacos%26hwtype%3Diphone%26tm%3D1480411885%26key%3D8147886b91774b159938af37a94a62da&s=4";
-                    // break;
-                } else if (title == "广东体育") {
-                    URL = "http%3A%2F%2F124.192.145.192%2Fm3u8%2Fguangdongtiyu_1300%2Fdesc.m3u8%3Fstream_id%3Dguangdongtiyu_1300%26path%3D124.192.145.211%2C124.192.145.83%2C124.192.145.154%26amltag%3D202%26mltag%3D202%26platid%3D10%26splatid%3D1003%26qos%3D4%26fcheck%3D0%26uid%3D2093832770.rp%26keyitem%3DdpoOd-l3AnupIUuBI8VtFwmKzWltcfnZpngeV5Yo3I1Oleh6yGrbVeKRoRQ.%26ntm%3D1480431000%26nkey2%3Dfeb3771b39490ce6e0a21e5652f3002f%26nkey%3D417f638fbb98588d013e33e7941255b9%26tag%3Dlive%26video_type%3Dm3u8%26useloc%3D0%26mslice%3D3%26uidx%3D0%26errc%3D0%26gn%3D149%26lrtmcd%3D109%26buss%3D202%26cips%3D124.205.90.66%26geo%3DCN-1-12-16%26tmn%3D1480412802%26pnl%3D149%2C149%2C398%2C224%26ext%3Dm3u8%26sign%3Dlive_web%26ostype%3Dmacos%26hwtype%3Diphone%26tm%3D1480412803%26key%3D8b47032625e85fa108669f2ffd476fec&s=4";
-                    // break;
-                } else if (title == "深圳体育") {
-                    URL = "http%3A%2F%2F124.192.145.203%2Fm3u8%2Fws_sztyjk_1300%2Fdesc.m3u8%3Fstream_id%3Dws_sztyjk_1300%26path%3D124.192.145.210%2C124.192.145.82%2C124.192.145.156%26amltag%3D202%26mltag%3D202%26platid%3D10%26splatid%3D1003%26qos%3D4%26fcheck%3D0%26uid%3D2093832770.rp%26keyitem%3DdpoOd-l3AnupIUuBI8VtFwmKzWltcfnZpngeV5Yo3I1Oleh6yGrbVeKRoRQ.%26ntm%3D1480431000%26nkey2%3D42844197aacd78ee829d1464e228b729%26nkey%3D2087665be98d5c62a86fa39e515c8e22%26tag%3Dlive%26video_type%3Dm3u8%26useloc%3D0%26mslice%3D3%26uidx%3D0%26errc%3D0%26gn%3D149%26lrtmcd%3D109%26buss%3D202%26cips%3D124.205.90.66%26geo%3DCN-1-12-16%26tmn%3D1480412885%26pnl%3D149%2C149%2C398%2C224%26ext%3Dm3u8%26sign%3Dlive_web%26ostype%3Dmacos%26hwtype%3Diphone%26tm%3D1480412885%26key%3Da469598e59d469c1841ffb6ffb9e96a6&s=4";
-                    // break;
-                }
+        for(var key in channels){
+            if(title==key){
+                URL=channels[key];
+                return;
             }
-        };
+        }
+
+        // for (key in channels) {
+        //     if (channels[key].type == 0) {
+        //         if (channels[key].title == title) {
+        //             URL = channels[key].url;
+        //             console.log("key", key);
+        //             console.log("url", URL);
+        //             console.log("type", channels[key].type);
+        //             break;
+        //         };
+        //     } else if (channels[key].type == 1) {
+        //         if (title == "风云足球") {
+        //             URL = "http%3A%2F%2F124.192.145.197%2Fm3u8%2Ffyzq_1300%2Fdesc.m3u8%3Fstream_id%3Dfyzq_1300%26path%3D124.192.145.194%2C124.192.145.66%2C124.192.145.154%26amltag%3D202%26mltag%3D202%26platid%3D10%26splatid%3D1003%26qos%3D4%26fcheck%3D0%26uid%3D2093832770.rp%26keyitem%3DdpoOd-l3AnupIUuBI8VtFwmKzWltcfnZpngeV5Yo3I1Oleh6yGrbVeKRoRQ.%26ntm%3D1480430400%26nkey2%3D6af0f495d57059bbc2ea9c8868d0851e%26nkey%3D2067e8bde51570a85ff460e0978b0971%26tag%3Dlive%26video_type%3Dm3u8%26useloc%3D0%26mslice%3D3%26uidx%3D0%26errc%3D0%26gn%3D149%26lrtmcd%3D109%26buss%3D202%26cips%3D124.205.90.66%26geo%3DCN-1-12-16%26tmn%3D1480411885%26pnl%3D149%2C149%2C398%2C224%26ext%3Dm3u8%26sign%3Dlive_web%26ostype%3Dmacos%26hwtype%3Diphone%26tm%3D1480411885%26key%3D8147886b91774b159938af37a94a62da&s=4";
+        //             // break;
+        //         } else if (title == "广东体育") {
+        //             URL = "http%3A%2F%2F124.192.145.192%2Fm3u8%2Fguangdongtiyu_1300%2Fdesc.m3u8%3Fstream_id%3Dguangdongtiyu_1300%26path%3D124.192.145.211%2C124.192.145.83%2C124.192.145.154%26amltag%3D202%26mltag%3D202%26platid%3D10%26splatid%3D1003%26qos%3D4%26fcheck%3D0%26uid%3D2093832770.rp%26keyitem%3DdpoOd-l3AnupIUuBI8VtFwmKzWltcfnZpngeV5Yo3I1Oleh6yGrbVeKRoRQ.%26ntm%3D1480431000%26nkey2%3Dfeb3771b39490ce6e0a21e5652f3002f%26nkey%3D417f638fbb98588d013e33e7941255b9%26tag%3Dlive%26video_type%3Dm3u8%26useloc%3D0%26mslice%3D3%26uidx%3D0%26errc%3D0%26gn%3D149%26lrtmcd%3D109%26buss%3D202%26cips%3D124.205.90.66%26geo%3DCN-1-12-16%26tmn%3D1480412802%26pnl%3D149%2C149%2C398%2C224%26ext%3Dm3u8%26sign%3Dlive_web%26ostype%3Dmacos%26hwtype%3Diphone%26tm%3D1480412803%26key%3D8b47032625e85fa108669f2ffd476fec&s=4";
+        //             // break;
+        //         } else if (title == "深圳体育") {
+        //             URL = "http%3A%2F%2F124.192.145.203%2Fm3u8%2Fws_sztyjk_1300%2Fdesc.m3u8%3Fstream_id%3Dws_sztyjk_1300%26path%3D124.192.145.210%2C124.192.145.82%2C124.192.145.156%26amltag%3D202%26mltag%3D202%26platid%3D10%26splatid%3D1003%26qos%3D4%26fcheck%3D0%26uid%3D2093832770.rp%26keyitem%3DdpoOd-l3AnupIUuBI8VtFwmKzWltcfnZpngeV5Yo3I1Oleh6yGrbVeKRoRQ.%26ntm%3D1480431000%26nkey2%3D42844197aacd78ee829d1464e228b729%26nkey%3D2087665be98d5c62a86fa39e515c8e22%26tag%3Dlive%26video_type%3Dm3u8%26useloc%3D0%26mslice%3D3%26uidx%3D0%26errc%3D0%26gn%3D149%26lrtmcd%3D109%26buss%3D202%26cips%3D124.205.90.66%26geo%3DCN-1-12-16%26tmn%3D1480412885%26pnl%3D149%2C149%2C398%2C224%26ext%3Dm3u8%26sign%3Dlive_web%26ostype%3Dmacos%26hwtype%3Diphone%26tm%3D1480412885%26key%3Da469598e59d469c1841ffb6ffb9e96a6&s=4";
+        //             // break;
+        //         }
+        //     }
+        // };
         $(".slogan").html("");
         $(".slogan").html("<div id='a1' class='animated fadeIn' style='width:100%;height:100%'></div>");
         var flashvars = {
