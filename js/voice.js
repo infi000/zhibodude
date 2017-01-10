bc7c4652bb527c607a77fb166e4fcf27f565f39d
infiooo <113079767@qq.com>
2017-01-10 18:47:10 +0800

    voice

--- js/voice.js
+++ js/voice.js
@@ -2,39 +2,43 @@
  * @Author: 张驰阳
  * @Date:   2017-01-09 17:34:07
  * @Last Modified by:   张驰阳
- * @Last Modified time: 2017-01-09 17:57:27
+ * @Last Modified time: 2017-01-10 15:15:35
  */
 
 'use strict';
 var voice = {};
-var data;
-function loadXMLDoc(url) {
-    var xmlhttp;
-    var txt, xx, x, i;
-    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
-    } else { // code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            x = xmlhttp.responseXML.documentElement.getElementsByTagName("channel");
-            console.log(x);
-        }
-    }
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+function reTime(t){
+    var t=new Date(t);
+    var n=new Date();
+    var result=(n.getTime()-t.getTime())/1000/60/60;
+    result=parseInt(result);
+    return result;
 }
+
 jQuery(document).ready(function($) {
     $.ajax({
-        url: 'http://voice.hupu.com/generated/voice/news_nba.xml',
-        // type:"GET",
-        dataType:"jsonp",
+        url: 'http://127.0.0.1:3000/hupu/1',
         success: function(msg) {
-        	data=msg;
-            console.log($(msg).find(".channel"));
+            var obj = "";
+            $(msg).find("channel item").map(function(index, elem) {
+                var title = $(elem).find("title").html();
+                var link = $(elem).find("link").html();
+                var des = $(elem).find("description").text();
+                var o=$("<div></div>");
+                o.html(des);
+                var voicePic=o.find("img").attr("src");
+                var info=o.find("p").eq(0).html();
+               info = info.substr(0, 45) + "...";
+                var date = $(elem).find("pubDate").html();
+                date=reTime(date);
+                obj += '<div class="voiceBox" type="button"  data-toggle="modal" data-target=".voiceArt" data-num="' + index + '">';
+                obj += '<div class="voiceSite"><img src="http://7xiyp4.com2.z0.glb.qiniucdn.com/voice.hupu.com" width="16px" height="16px"><span>虎扑篮球新声</span></div>';
+                obj += '<div class="voiceTitle">' + title + '</div><div class="voicePic"><img src="' + voicePic + '" width="100%"></div>';
+                obj += '<div class="voiceView"><p>' + info + '</p></div>';
+                obj += '<div class="voiceTime"><span class="glyphicon glyphicon-time"></span><span>' + date + '小时前</span></div> </div>';
+                return obj;
+            });
+           $("#voiceContainer").html(obj);
         }
-    })
-// loadXMLDoc('http://voice.hupu.com/generated/voice/news_nba.xml');
-
+    });
 });
